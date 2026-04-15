1dbc5cb8d83432567fbe109fc24f8a0dfc1d334a
tzk07 <2499307660@qq.com>
2026-04-15 14:57:10 +0800

    Effect Actor Improved

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -3,48 +3,57 @@
 
 #include "Actor/AuraEffectActor.h"
 
-#include "AbilitySystemInterface.h"
+#include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystem/AuraAttributeSet.h"
-#include "Components/SphereComponent.h"
 
 // Sets default values
 AAuraEffectActor::AAuraEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
 	
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
-void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool FromSweep, const FHitResult& SweepResult)
-{
-	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		// UAuraAttributeSet::StaticClass(): 静态调用。不需要实例，直接通过类名获取该类的类型信息。
-		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
-		UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
-		MutableAuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.f);
-		MutableAuraAttributeSet->SetMana(AuraAttributeSet->GetMana() - 10.f);
-		Destroy();
-	}	
-}
-
-void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	
-}
 
 void AAuraEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+/**
+ * 
+ * @param Target 效果的接收者
+ * @param GameplayEffectClass 效果的创建模板 
+ */
+void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	/*
+		IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(Target);
+		if (ASCInterface)
+		{
+			 * 更快一点，确定目标实现了 GetAbilitySystemComponent() 接口
+			 * ASCInterface->GetAbilitySystemComponent();
+			 * 通过“搜索”访问。我不确定你在哪，但我会翻遍你的全身（接口、组件、关联类）把你找出来
+			 * UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+		}
+	*/
+	// 从 Target 上获取 ASC
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if (TargetASC == nullptr) return;
 	
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
+	check(GameplayEffectClass);
+	
+	// ContextHandle 轻量级句柄，存储 Context 的指针，记录了这次效果触发时的环境信息
+	// 主要用于逻辑判定（比如：如果伤害来源是队友，则减免伤害）和表现（比如：在被命中的那个点播放粒子特效）
+	// 可以自定义扩展
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	// 明确标记这个效果的 发出者
+	EffectContextHandle.AddSourceObject(this);
+	
+	// EffectSpec 存储了经过计算后的最终指令数据
+	// Spec 包含了 Context
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
 
