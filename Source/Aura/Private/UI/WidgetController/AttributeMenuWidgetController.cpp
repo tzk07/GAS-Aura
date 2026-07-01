fd335d43f82fceb49838e10230035b30db770177
tzk07 <2499307660@qq.com>
2026-07-01 21:21:58 +0800

    Attribute Info Delegate

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -2,10 +2,19 @@
 
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	UAuraAttributeSet* AS = Cast<UAuraAttributeSet>(AttributeSet);
 	
+	check(AttributeInfo);
+	// 设置 Strength 的值: 从 系统 获取标签 -> 通过标签查找 AttributeInfo -> 修改成员变量 AttributeValue
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AS->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -41,7 +41,10 @@ class AURA_API UAuraWidgetController : public UObject
 public:
 	UFUNCTION(BlueprintCallable)
 	void SetWidgetControllerParams(const FWidgetControllerParams& WCParams);
+	
+	UFUNCTION(BlueprintCallable) 
 	virtual void BroadcastInitialValues();
+	
 	virtual void BindCallbacksToDependencies();
 	
 protected:
